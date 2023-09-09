78a2ae9e61c53fe67faa9834ca37a1b6bd926b5e
SirmaXX <sea-97@hotmail.com>
2023-09-09 04:25:45 +0300

    ip whitelist eklendi

--- ApiGateway/app/main.py
+++ ApiGateway/app/main.py
@@ -4,6 +4,7 @@ import starlette.status as status
 import urllib,json,requests
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 from typing import Optional,List
 from app.Routers.restapi import restapiroute
@@ -14,12 +15,14 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 
 
-
+#TrustedHostMiddleware ip whitelist için kullanılır
 
 def create_app():
   limiter = Limiter(key_func=get_remote_address)
   app = FastAPI(title="apigateway", description="apigateway")
   app.state.limiter = limiter
+#  app.add_middleware(TrustedHostMiddleware, allowed_hosts=[""] )
+
   app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
   app.include_router(restapiroute, prefix="/v1/api")
   return app
